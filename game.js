b5104fb55d72084a9a2f3eeff13578033e361d6f
Erik <eculberson12@gmail.com>
2016-05-11 21:28:14 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,27 @@
+//var canvas, context;
+//var width, height;
+
+function main() {
+  canvas = document.createElement("canvas");
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
+  context = canvas.getContext("2d");
+  document.body.appendChild(canvas);
+}
+/*
+function run() {
+}
+
+function update() {
+}
+
+function render() {
+}
+
+*/
+main();
+
+
+*/
